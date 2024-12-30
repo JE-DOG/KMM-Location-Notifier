b6f5b6f39548e29a07b0d8cc6f9ee9050f3f7d96
JE_DOG <gamzat.magomedov809@gmail.com>
2024-12-30 22:35:32 +0300

    Добавил locationService на андроид

--- composeApp/src/androidMain/kotlin/location/AndroidServiceLocation.kt
+++ composeApp/src/androidMain/kotlin/location/AndroidServiceLocation.kt
@@ -1,16 +1,36 @@
 package location
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import ru.khinkal.locationNotifier.LocationNotifierApp
+import ru.khinkal.locationNotifier.core.location.LocationManagerImpl
 import ru.khinkal.locationNotifier.core.location.model.BaseGeoPoint
 
 actual fun getLocationService(): LocationServiceVariants = object : LocationServiceVariants {
+
+    private val coroutineScope = CoroutineScope(SupervisorJob())
+    private val application = LocationNotifierApp.INSTANCE
+    private val locationManager by lazy { LocationManagerImpl(application) }
+    private var locationBroadcastJob: Job? = null
+
     override fun startBroadcast(
         interval: Double,
         onLocationUpdated: (BaseGeoPoint) -> Unit,
     ) {
-        TODO("Not yet implemented")
+        locationBroadcastJob?.cancel()
+        locationBroadcastJob = coroutineScope.launch {
+            locationManager.broadcastLocation(
+                interval.toInt(),
+            )
+                .collect { baseGeoPoint ->
+                    onLocationUpdated(baseGeoPoint)
+                }
+        }
     }
 
     override fun stopBroadcast() {
-        TODO("Not yet implemented")
+        locationBroadcastJob?.cancel()
     }
 }
