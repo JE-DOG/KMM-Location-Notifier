92d05018f5d8e6ce48c771824a7f446642f51ed3
JE_DOG <gamzat.magomedov809@gmail.com>
2024-12-30 22:35:13 +0300

    Убрал лишнее

--- composeApp/src/commonMain/kotlin/ru/khinkal/locationNotifier/App.kt
+++ composeApp/src/commonMain/kotlin/ru/khinkal/locationNotifier/App.kt
@@ -2,20 +2,13 @@ package ru.khinkal.locationNotifier
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import location.getLocationService
 import ru.khinkal.locationNotifier.navigation.AppHost
 import ru.khinkal.locationNotifier.shared.theme.AppTheme
 
 @Composable
 internal fun App() {
     AppTheme {
-        LaunchedEffect(Unit) {
-            getLocationService().startBroadcast(10.0, {
-
-            })
-        }
         AppHost(
             modifier = Modifier
                 .fillMaxSize(),
