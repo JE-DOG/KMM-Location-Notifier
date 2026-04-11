fd4f5953f9fdd675ebcf8443b93fe7a883a69e6f
JE_DOG <gamzat.magomedov809@gmail.com>
2026-04-11 23:42:03 +0300

    Update libs versions

--- convention-plugins/base/src/main/java/ru/khinkal/locationNotifier/convention_plugins/base/ext/android/AndroidExt.kt
+++ convention-plugins/base/src/main/java/ru/khinkal/locationNotifier/convention_plugins/base/ext/android/AndroidExt.kt
@@ -1,32 +1,17 @@
 package ru.khinkal.locationNotifier.convention_plugins.base.ext.android
 
-import com.android.build.api.dsl.AndroidResources
-import com.android.build.api.dsl.BuildFeatures
-import com.android.build.api.dsl.BuildType
+import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.api.dsl.DefaultConfig
-import com.android.build.api.dsl.Installation
 import com.android.build.api.dsl.LibraryExtension
-import com.android.build.api.dsl.ProductFlavor
-import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.findByType
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompilerOptions
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import ru.khinkal.locationNotifier.convention_plugins.base.ext.multiplatform.kotlinMultiplatformExt
 
-private typealias AndroidExtensions = CommonExtension<
-        out BuildFeatures,
-        out BuildType,
-        out DefaultConfig,
-        out ProductFlavor,
-        out AndroidResources,
-        out Installation>
+private typealias AndroidExtensions = CommonExtension
 
 private val Project.androidExtension: AndroidExtensions
-    get() = extensions.findByType(BaseAppModuleExtension::class)
+    get() = extensions.findByType(ApplicationExtension::class)
         ?: extensions.findByType(LibraryExtension::class)
         ?: error(
             "\"Project.androidExtension\" value may be called only from android application" +
@@ -37,10 +22,4 @@ fun Project.androidConfig(block: AndroidExtensions.() -> Unit): Unit = block(and
 
 fun Project.multiplatformAndroidConfig(block: KotlinAndroidTarget.() -> Unit) {
     kotlinMultiplatformExt.androidTarget(configure = block)
-}
-
-fun Project.kotlinJvmCompilerOptions(block: KotlinJvmCompilerOptions.() -> Unit) {
-    tasks.withType<KotlinJvmCompile>().configureEach {
-        compilerOptions(block)
-    }
 }
